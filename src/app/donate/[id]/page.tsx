f25a82728384fc235440c693e90b9032978bcf2d
puravgc <puravgc02@gmail.com>
2025-01-02 16:00:24 +0545

    updates

--- src/app/donate/[id]/page.tsx
+++ src/app/donate/[id]/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const params = useParams();
+  const { id } = params;
+  console.log(id);
+  useEffect(() => {
+    const getRequestDetails = async () => {
+      const response = await fetch(`/api/get-request/${id}`);
+      const data = await response.json();
+      console.log(data);
+    };
+    getRequestDetails();
+  }, []);
 
-export default page
+  return <div>page</div>;
+};
+
+export default page;
